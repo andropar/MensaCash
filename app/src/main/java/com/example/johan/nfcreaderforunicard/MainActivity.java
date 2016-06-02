3f48398f914ecef3bae8facc423f9d3221384299
Johannes Roth <johannes@roth24.de>
2016-06-02 10:42:19 +0200

    Fixed rotation and connection issues

--- app/src/main/java/com/example/johan/nfcreaderforunicard/MainActivity.java
+++ app/src/main/java/com/example/johan/nfcreaderforunicard/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.johan.nfcreaderforunicard;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    // Storage-Bytes for InterCard
+    private final byte[] selectAid = {(byte)90, (byte)95, (byte)-124, (byte)21};      //select application command
+    private final byte[] creditPayload = {(byte)108, (byte)1};                        //select credit file
+
+    private byte[] resultOk;
+    private byte[] creditBytes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +36,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+        if(NfcAdapter.ACTION_TECH_DISCOVERED.equals(getIntent().getAction())){
+            IsoDep isodep = IsoDep.get((Tag)getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG));
+            if(isodep != null){
+                try{
+                    isodep.connect();
+                    resultOk = isodep.transceive(selectAid);
+                    if(resultOk[0] == 0){
+                        creditBytes = isodep.transceive(creditPayload);
+                    }
+                }catch (IOException e){
+                }
+            }
+            float credit = (float)formatCreditToDouble(creditBytes);
+            MensaMenuFragment m_frag = (MensaMenuFragment)getSupportFragmentManager().findFragmentById(R.id.MensaMenu);
+            m_frag.updateMenuPositions(credit);
+            TextView label = (TextView)findViewById(R.id.main_text);
+            label.setText("Guthaben: "+round(credit, 2)+"€");
+        }
+    }
+
+    private double formatCreditToDouble(byte[] array) {
+        double credit = (double)(((0xff & array[4]) << 24) + ((0xff & array[3]) << 16) + ((0xff & array[2]) << 8) + (0xff & array[1])) / 1000D;
+        return credit;
+    }
+
+    private static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+    @Override
+    public void onStop(){
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+        super.onStop();
     }
 
     @Override
--- app/src/main/java/com/example/johan/nfcreaderforunicard/MensaMenuFragment.java
+++ app/src/main/java/com/example/johan/nfcreaderforunicard/MensaMenuFragment.java
@@ -2,6 +2,9 @@ package com.example.johan.nfcreaderforunicard;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -11,6 +14,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -26,6 +31,11 @@ import java.util.List;
  */
 public class MensaMenuFragment extends ListFragment {
 
+    private static final String TAG = "MensaMenuFragment";
+
+    List<String> data = new ArrayList<String>();
+    List<String> priceList = new ArrayList<String>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.menufragment_view, container, false);
@@ -38,52 +48,84 @@ public class MensaMenuFragment extends ListFragment {
         new MenuTask().execute();
     }
 
-    private class MenuTask extends AsyncTask<List<String>, String, List<String>>{
-        List<String> data = new ArrayList<String>();
+    public void updateMenuPositions(double credit){
+        Log.v(TAG, "Combine lists");
+        List<String> list = combineLists(data, priceList);
+        Log.v(TAG, "Create new array adapter");
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, list){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                TextView text = (TextView) super.getView(position, convertView, parent);
+                text.setBackgroundColor(Color.GREEN);
+                return text;
+            }
+        };
+        Log.v(TAG, "Set listadapter");
+        setListAdapter(adapter);
+    }
 
+    private class MenuTask extends AsyncTask<List<String>, String, List<String>>{
         @Override
         protected List<String> doInBackground(List<String>... params){
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            Date date = new Date();
+            ConnectivityManager cm =
+                    (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            if(netInfo != null && netInfo.isConnected()){
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                Date date = new Date();
 
-            SharedPreferences sPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
-            String loc = sPref.getString("mensa_key", "106");
+                SharedPreferences sPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                String loc = sPref.getString("mensa_setting", "106");
 
-            String url = "http://www.studentenwerk-leipzig.de/mensa/menu?date="+
-                    dateFormat.format(date)+loc;
-            try{
-                Document doc = Jsoup.connect(url).get();
-                Elements menus = doc.getElementsByClass("menu");
-                for(Element menu: menus){
-                    if(menu.getElementsByClass("menu_price").hasText()){
-                        Elements food = menu.getElementsByClass("col_name");
-                        Elements price = menu.getElementsByClass("menu_price");
-                        String text = "";
-                        for(Element foods: food){
-                            Elements e = foods.children();
-                            for(Element singleFood: e){
-                                text += singleFood.text()+"\n";
+                String url = "http://www.studentenwerk-leipzig.de/mensa/menu?date=" +
+                        dateFormat.format(date) + "&location=" + loc;
+                Log.v(TAG, "URL:" + url);
+                try {
+                    Document doc = Jsoup.connect(url).get();
+                    Elements menus = doc.getElementsByClass("menu");
+                    for (Element menu : menus) {
+                        if (menu.getElementsByClass("menu_price").hasText()) {
+                            Elements food = menu.getElementsByClass("col_name");
+                            Elements price = menu.getElementsByClass("menu_price");
+                            String text = "";
+                            for (Element foods : food) {
+                                Elements e = foods.children();
+                                for (Element singleFood : e) {
+                                    text += singleFood.text() + "\n";
+                                }
                             }
+                            String[] prices = price.text().split("€");
+                            prices[1] = prices[1].replace("/", "").trim();
+                            prices[2] = prices[2].replace("/", "").trim();
+                            int i = Integer.parseInt(sPref.getString("state_setting", "0"));
+                            priceList.add(prices[i]);
+                            data.add(text);
                         }
-                        String[] prices = price.text().split("€");
-                        text += prices[0] + "€" + prices[1] + "€" + prices[2] + "€";
-                        data.add(text);
                     }
-                }
-                return data;
-            }catch(Exception e){
-                Log.v("MenuFragment", e.toString());
+                    return data;
+                } catch (Exception e) {
+                    Log.v("MenuFragment", e.toString());
 
+                }
             }
             return null;
         }
 
         @Override
         protected void onPostExecute(List<String> data){
-            ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, data);
+            List<String> list = combineLists(data, priceList);
+            ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, list);
             setListAdapter(adapter);
         }
+
+
     }
 
-
+    private List<String> combineLists(List<String> a, List<String> b){
+        List<String> list = new ArrayList<String>();
+        for(int i = 0; i < b.size(); i++){
+            list.add(a.get(i) + b.get(i) + "€");
+        }
+        return list;
+    }
 }
